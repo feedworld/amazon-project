a99fed8cedd9b1469a8dacc9458e7aeaf1183f05
Feed World <hopefeedtheworld@gmail.com>
2025-08-20 08:27:05 -0700

    Make Cart Quantity Interactive

--- script/amazon.js
+++ script/amazon.js
@@ -73,8 +73,21 @@ products.forEach((product)=>{
               })
 
               }
+              let cartQuantity = 0;
+
+              cart.forEach((item)=>{
+
+                cartQuantity += item.quantity
+
+              })
+
+
+              document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
+
+
              
-              console.log(cart)
+            
             })
         })
 
