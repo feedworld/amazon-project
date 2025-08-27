942d5910a22996d2bb852432440c6400177d1ecd
Feed World <hopefeedtheworld@gmail.com>
2025-08-27 01:41:37 -0700

    delete product from cart when click delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 
 
-export const cart =[ {
+export let cart =[ {
     productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
     quantity: 1
 }, {
@@ -30,4 +30,15 @@ export function addToCart(productId){
 
               }
     
+};
+
+export function removeFromCart(productI){
+  const newCart = [];
+
+  cart.forEach((cartItem) =>{
+    if(cartItem.productId !== productI){
+      newCart.push(cartItem)
+    }
+  })
+  cart = newCart
 }
