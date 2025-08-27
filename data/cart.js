7e78464fce822f179748ba041ff4bb413a530d22
Feed World <hopefeedtheworld@gmail.com>
2025-08-27 02:32:59 -0700

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,24 @@
 
 
-export let cart =[ {
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart = 
+[ {
     productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
     quantity: 1
 }, {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 }];
+
+}
+
+
+
+
+function savetoStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 
 
@@ -29,8 +41,12 @@ export function addToCart(productId){
               })
 
               }
+              savetoStorage()
     
 };
+
+
+
 
 export function removeFromCart(productI){
   const newCart = [];
@@ -41,4 +57,7 @@ export function removeFromCart(productI){
     }
   })
   cart = newCart
-}
+
+  savetoStorage()
+};
+
