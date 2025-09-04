448cbfbf622a9f375492c0c61fbd3b29ffb9468c
Feed World <hopefeedtheworld@gmail.com>
2025-09-04 05:20:33 -0700

    Save date and generate html

--- data/cart.js
+++ data/cart.js
@@ -5,20 +5,23 @@ if(!cart){
   cart = 
 [ {
     productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '1'
 }, {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '2'
 }];
 
+
 }
-
-
-
-
 function savetoStorage(){
   localStorage.setItem('cart', JSON.stringify(cart))
 }
+
+
+
+
 
 
 
@@ -37,7 +40,8 @@ export function addToCart(productId){
               }else {
                  cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: 1,
+                deliveryOptionId: '1'
               })
 
               }
@@ -48,16 +52,12 @@ export function addToCart(productId){
 
 
 
-export function removeFromCart(productI){
-  const newCart = [];
+export function removeFromCart(productI) {
+  const index = cart.findIndex(cartItem => cartItem.productId === productI);
 
-  cart.forEach((cartItem) =>{
-    if(cartItem.productId !== productI){
-      newCart.push(cartItem)
-    }
-  })
-  cart = newCart
+  if (index !== -1) {
+    cart.splice(index, 1); 
+  }
 
-  savetoStorage()
-};
-
+  savetoStorage();
+}
