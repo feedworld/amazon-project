c911edeadebe4b371a23cf9976a2e59e06e9ad06
Feed World <hopefeedtheworld@gmail.com>
2025-09-25 06:02:32 -0700

    remove default tests and add jasmine test

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,20 @@
+import {formatCurrency} from '../script/utils/money.js';
+
+
+
+
+describe('test suite: formatCurrency', ()=>{
+    it ('Convert cents into Dollar', ()=>{
+    expect(formatCurrency('2095')).toEqual('20.95');
+    });
+
+
+it('works with 0', ()=>{
+    expect(formatCurrency(0)).toEqual('0.00')
+});
+
+
+it('rounds to the nearest cent',()=>{
+    expect(formatCurrency(2000.5)).toEqual('20.01')
+})
+})
