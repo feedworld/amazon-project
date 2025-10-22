6b5b8c931fadbdb7078b6bd7b41fc44e7df78d4c
Feed World <hopefeedtheworld@gmail.com>
2025-10-22 09:52:15 -0700

    changed products into class

--- script/checkout/ordersummary.js
+++ script/checkout/ordersummary.js
@@ -49,7 +49,7 @@ export function renderOrderSummary() {
         </div>
 
         <div class="product-price">
-          ${formatCurrency(matchingProduct.priceCents)}
+        ${matchingProduct.getPrice()}
         </div>
 
         <div class="product-quantity  js-roduct-quantity-${matchingProduct.id}">
