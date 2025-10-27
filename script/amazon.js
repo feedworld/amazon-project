084c0f97d01aa6ef6ebfc4f3455932d3a20b79e5
Feed World <hopefeedtheworld@gmail.com>
2025-10-27 13:47:15 -0700

    Use inheritance and polymorphism to add size chat

--- script/amazon.js
+++ script/amazon.js
@@ -56,6 +56,8 @@ products.forEach((product)=>{
             </select>
           </div>
 
+          ${product.extraInfoHTML()}
+
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
