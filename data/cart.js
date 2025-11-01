83d19ae674aa197c5c5025e6f26c16b0ee31809a
Feed World <hopefeedtheworld@gmail.com>
2025-11-01 07:38:10 -0700

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -87,3 +87,21 @@ export function updateDeliveryOption(productId, deliveryOptionId){
               matchingItem.deliveryOptionId = deliveryOptionId;
 
 savetoStorage(); }
+
+
+
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    
+    console.log(xhr.response)
+    fun()
+
+
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+};
--- script/checkout.js
+++ script/checkout.js
@@ -1,11 +1,57 @@
-import {renderOrderSummary} from './checkout/ordersummary.js';
+import { renderOrderSummary } from './checkout/ordersummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
 import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
+Promise.all([
+    new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    })
+}),new Promise((resolve)=>{
+    loadCart(()=>{
+        resolve();
+    })
+  }).then(()=>{
+     renderOrderSummary();
+    renderPaymentSummary()
 
-loadProducts(() =>{
-renderOrderSummary();
-renderPaymentSummary();
+  })
+
+])
+
+/*
+
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    })
+}).then((value) => {
+    
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+        resolve();
+    })
+  }).then(()=>{
+     renderOrderSummary();
+    renderPaymentSummary()
+
+  })
+
 })
+  */
+    
+
+/*
+loadProducts(() => {
+    loadCart(()=>{
+     renderOrderSummary();
+    renderPaymentSummary();
+
+    })
+    
+
+})
+*/
